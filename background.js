d8e83fd7d57b5c53815500e7f6b58591154a2f67
danielibvi <133038080+danielibvi@users.noreply.github.com>
2023-05-10 19:24:32 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,29 @@
-{chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == "generateAvatar") {
-    fetch("https://thispersondoesnotexist.com/image", {
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+document.addEventListener("DOMContentLoaded", function() {
+  try {
+    document.getElementById("generate-avatar").addEventListener("click", function() {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "generateAvatar" });
+      });
+    });
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (request.action == "generateAvatar") {
+        fetch("https://thispersondoesnotexist.com/image", {
+          headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+          }
+        })
+        .then(response => response.blob())
+        .then(blob => {
+          const url = URL.createObjectURL(blob);
+          sendResponse({ avatarUrl: url });
+        })
+        .catch(error => console.log(error));
+        return true;
       }
-    })
-    .then(response => response.blob())
-    .then(blob => {
-      const url = URL.createObjectURL(blob);
-      sendResponse({ avatarUrl: url });
-    })
-    .catch(error => console.log(error));
-    return true;
+    });
+
+  } catch (error) {
+    console.log(error);
   }
-});
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'aia.com' },
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
 });
